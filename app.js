e79a1aa2fc3ae5365710950b2bca2d091cd740f8
Jake Siney <jakesiney@gmail.com>
2023-09-17 15:20:48 +0100

    Project setup

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let squares = document.querySelectorAll(".grid div");
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-button");
+  const width = 10;
+});
